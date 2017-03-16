65b05bf611e2ab44575200093318ac3cc73de952
Igor Stojanovski <igorce@outlook.com>
2017-03-16 22:06:10 +0100

    Basic Setup

---
+++ src/main/java/com/codekasteel/AtroposApplication.java
@@ -0,0 +1,12 @@
+package com.codekasteel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtroposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtroposApplication.class, args);
+	}
+}
